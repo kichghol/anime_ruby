069fed67b8a21e93eef0b4fa47a68a578372a959
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-27 12:07:09 +0100

    Sign in & sign up only when not connected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_connected
+
+  private
+
+  def set_connected()
+    @cache = ActiveSupport::Cache::MemoryStore.new() if @cache.nil?
+    token = Rails.cache.read('currentToken')
+    if token
+      @connected=true
+    else
+      @connected=false
+    end
+  end
+
+
 end
